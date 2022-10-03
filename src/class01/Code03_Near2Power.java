345596a4ac7991715262e22cc8e29a0b5ff56bba
gqdeng <gqdeng@cn.ibm.com>
2022-10-03 17:46:53 +0800

    返回离非负整数num最近的2的某次方

--- src/class01/Code03_Near2Power.java
+++ src/class01/Code03_Near2Power.java
@@ -1,5 +1,25 @@
 package class01;
 
 public class Code03_Near2Power {
+	
+	/**
+	 * 返回离非负整数num最近的2的某次方 (这题没多大意思)
+	 */
+	// 已知n是正数
+	// 返回大于等于，且最接近n的，2的某次方的值
+	public static final int tableSizeFor(int n) {
+		n--;
+		n |= n >>> 1;
+		n |= n >>> 2;
+		n |= n >>> 4;
+		n |= n >>> 8;
+		n |= n >>> 16;
+		return (n < 0) ? 1 : n + 1;
+	}
+
+	public static void main(String[] args) {
+		int cap = 120;
+		System.out.println(tableSizeFor(cap));
+	}
 
 }
