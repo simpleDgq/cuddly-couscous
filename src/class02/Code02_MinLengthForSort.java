d9c87fd8fe8673a336eeca356cc1138fac084d96
gqdeng <gqdeng@cn.ibm.com>
2022-10-06 17:52:40 +0800

    从左往右扫描，从右往左扫描，分别确定不用排序的点--无序数组需要排序的最短子数组长度

---
+++ src/class02/Code02_MinLengthForSort.java
@@ -0,0 +1,52 @@
+package class02;
+
+public class Code02_MinLengthForSort {
+	
+	/**
+	 * 本题测试链接 : https://leetcode.com/problems/shortest-unsorted-continuous-subarray/
+	 * 
+	 * 无序数组需要排序的最短子数组长度 - 时间复杂度 O(N)额外空间复杂度O(1)
+	 * 
+	 * 思路:
+	 * 1）先从左往右遍历,
+	 *		max左: 划过部分的最大值
+	 *		左Max>当前数字: 打叉
+	 *		左Max<=当前数字: 打勾
+	 * 记录最右打叉的位置
+	 * 2）然后从右往左遍历,
+	 *		min右: 划过部分的最小值
+	 *		右Min<当前数字: 打叉
+	 *		右Min>=当前数字: 打勾
+	 * 记录最左打叉的位置
+	 * 
+	 * 两个打叉的位置，表示的范围就是需要排序的数组。
+	 */
+	public static int findUnsortedSubarray(int nums[]) {
+		if(nums == null || nums.length < 2) {
+			return 0;
+		}
+		
+		int leftMax = nums[0];
+		int rightPoint = 0;
+		for(int i = 1; i <= nums.length - 1; i++) {
+			if(leftMax > nums[i]) {
+				rightPoint = i;
+			} else {
+				leftMax = nums[i];
+			}
+		}
+		
+		int rightMin = nums[nums.length - 1];
+		int leftPoint = nums.length - 1;
+		for(int i = nums.length - 2; i >= 0; i--) {
+			if(rightMin < nums[i]) {
+				leftPoint = i;
+			} else {
+				rightMin = nums[i];
+			}
+		}
+		
+		return Math.max(0, rightPoint - leftPoint + 1);
+	}
+
+}
