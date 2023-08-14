c1fe29183dfa2a50e4a5415243a0f18a113a1a96
gqdeng <gqdeng@cn.ibm.com>
2023-08-14 17:15:37 +0800

    拼多多，卷子合法数量问题 - 略

---
+++ src/class32/Problem_0166_FractionToRecurringDecimal.java
@@ -0,0 +1,23 @@
+package class32;
+
+//166. 分数到小数
+public class Problem_0166_FractionToRecurringDecimal {
+    /**
+     * 给定两个整数，分别表示分数的分子 numerator 和分母 denominator，以 字符串形式返回小数 。
+     * 如果小数部分为循环小数，则将循环的部分括在括号内。
+     * 
+     * 如果存在多个答案，只需返回 任意一个 。
+     * 对于所有给定的输入，保证 答案字符串的长度小于 104 。
+     * 
+     * 输入：numerator = 1, denominator = 2
+     * 输出："0.5"
+     * 
+     * 输入：numerator = 4, denominator = 333
+     * 输出："0.(012)"
+     */
+    
+    /**
+     * 这题感觉纯数学问题
+     * 
+     */
+}
