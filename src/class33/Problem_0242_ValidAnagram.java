ec296be7f818997bb48ae7f5593edfb64652ac7b
GuoQingDeng <1255826318@qq.com>
2024-11-08 19:57:16 +0800

    242. 有效的字母异位词 - 数组统计字符个数

--- src/class33/Problem_0242_ValidAnagram.java
+++ src/class33/Problem_0242_ValidAnagram.java
@@ -9,31 +9,32 @@ public class Problem_0242_ValidAnagram {
      */
     
     /**
-     * 思路:
      * 
-     * 如果长度不相等，一定不是异位词；
-     * 如果长度相等，字符串1每种字符 ++的次数，
-     * 对于字符串2每一个字符，都没有--到小于0的时刻，
-     * 说明字符数一样，种类也一样
+     * 思路：
+     * 1.如果两个字符串长度不相等，那么必然不是字母异位词
+     * 2.搞一个数组count记录s中每个字符出现的次数, 遍历s填好
+     * 3.遍历t，对count中对应的字符数量进行--，--之后如果出现了次数小于0的情况，那么一定不是字母异位词
+     * 
+     * 也可以用hashmap
+     * 或者排序之后，看是否相等，Arrays.equals()
      */
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) {
+        if (s.length() != t.length()) {
             return false; // 如果长度不相等
         }
         // 如果长度相等
         char s1[] = s.toCharArray();
         char t1[] = t.toCharArray();
-        
-        int chars[] = new int[256];
-        for(char ch: s1) {
-            chars[ch]++; // 字符串每种字符都++
+
+        int count[] = new int[256];
+        for (char ch : s1) {
+            count[ch]++; // 字符串每种字符都++
         }
-        
         // 字符串2，每种字符都--，所有字符如果--都没有小于0的时刻
         // 而且两者长度相等，说明两个字符串长度相等，字符数量相同
-        for(char ch: t1) {
-            chars[ch]--;
-            if(chars[ch] < 0) {
+        for (char ch : t1) {
+            count[ch]--;
+            if (count[ch] < 0) {
                 return false; // 有小于0的时刻，说明某个字符，数量不相等
             }
         }
